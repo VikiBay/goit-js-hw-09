746ffbaa4d0d8f04cdf1ac570999aa41aec0e568
Viktoria Bayer <bayer.skaya@gmail.com>
2022-11-13 22:07:56 +0100

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,5 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-form = document.querySelector('.form')
+const form = document.querySelector('.form')
 const {delay,step,amount} = form.elements;
 
 form.addEventListener("submit", onFormSubmit)
